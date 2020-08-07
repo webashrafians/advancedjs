a6210dc414cbe4106f16d22f9271f08f99bc11f1
webashrafians <webashrafians@gmail.com>
2020-08-07 21:11:04 +0600

    true false

---
+++ truefalse.js
@@ -0,0 +1,20 @@
+//0
+//""
+//undefined
+//null
+//NaN
+// ....................... false value
+
+//"0"
+//" "
+//[]
+// ......................... true value
+
+// const name = "Solaiman Khan";
+let name = null;
+console.log(name);
+if(name){
+    console.log("Condition is true");
+} else{
+    console.log("Condition is false");
+}

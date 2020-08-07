1ff54ee13b3a2192dfc519648b18b75e92e4d9d7
webashrafians <webashrafians@gmail.com>
2020-08-07 21:22:47 +0600

    undefined vs nul

---
+++ null-vs-undefined.js
@@ -0,0 +1,18 @@
+// let pakhi; // undefined
+// function add(num1, num2){
+//     console.log(num1 + num2);
+//     return
+// } //undefined
+
+// function add(num1, num2){
+//     console.log(num1, num2);
+// }
+
+// const premik = {name: "smart phone", phone: 4545425};
+
+// const result = add(13)
+// console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+

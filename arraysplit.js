e7b2294a9ce70cdbcce67acbc7ceb2686047eab2
webashrafians <webashrafians@gmail.com>
2020-08-09 21:38:16 +0600

    final

---
+++ arraysplit.js
@@ -0,0 +1,10 @@
+// var nums = [1,2,3,4,5,6,7,8];
+// // const part = nums.slice(2,5);
+
+// const removed = nums.splice(3,4, 77, 88, 99);
+// // console.log(removed);
+// // console.log(nums);
+
+// const together = nums.join(", ");
+// console.log(together);
+
---
+++ scope.js
@@ -0,0 +1,18 @@
+let bonus = 20;
+function sum(first, second){
+    let result = first + second + bonus;
+    console.log(bonus);
+
+    if (result > 9) {
+        const mood = "Happy";
+        console.log(mood);
+        console.log(dunky);
+        console.log(monky);
+    }
+
+    return result;
+}
+
+const output = sum(3,7);
+console.log(bonus);
+console.log(output);
